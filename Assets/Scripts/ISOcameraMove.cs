14f39020cc384c2c2efe4829865e3013fb190109
jbishop-art <jonathan.bishop.art@gmail.com>
2018-08-21 22:14:29 -0600

    Intial scene.  Camera & player01 movement with controller or keyboard
    Intial scene.  Camera & player01 movement with controller or keyboard

---
+++ Assets/Scripts/ISOcameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ISOcameraMove : MonoBehaviour 
+{
+	public GameObject Player01;
+	float playerX;
+	float playerZ;
+	public float cameraOffsetX;
+	public float cameraOffsetZ;
+	public float cameraOffsetY;
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		playerX = Player01.transform.position.x;
+		playerZ = Player01.transform.position.z;
+
+
+		transform.position = new Vector3((playerX + cameraOffsetX), cameraOffsetY, (playerZ + cameraOffsetZ));
+
+	}
+}
---
+++ Assets/Scripts/Player01Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player01Controller : MonoBehaviour 
+{
+	public float moveSpeed;
+	Vector3 forward, right;
+
+	// Use this for initialization
+	void Start () 
+	{
+		forward = Camera.main.transform.forward;
+		forward.y = 0;
+		forward = Vector3.Normalize(forward);
+
+		right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Detect Input.
+		if (Input.anyKey)
+			Move();
+	}
+
+	//Drives direction of object based on key input.
+	void Move()
+	{
+		//Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+		//Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+		Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Left Thumb Horizontal");
+		Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Left Thumb Vertical");
+
+		Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+		transform.forward = heading;
+		transform.position += rightMovement;
+		transform.position += upMovement;
+
+	}
+}
