5ef396dd71eaa72dc4cd7fa73b52a8936a32fa51
RikkiRun <31293953+RikkiRun@users.noreply.github.com>
2018-08-28 19:50:56 -0600

    implement animation

---
+++ Assets/Scripts/Animation/Facing_Direction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Facing_Direction : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+	}
+
+    public void switchAnimation(string s)
+    {
+        GetComponent<Animator>().SetTrigger(s);
+    }
+    public void leftMove()
+    {
+        GetComponent<Animator>().SetTrigger("leftMoveTrigger");
+    }
+
+    public void rightMove()
+    {
+        GetComponent<Animator>().SetTrigger("rightMoveTrigger");
+    }
+
+    public void turnAround()
+    {
+        GetComponent<Animator>().SetTrigger("turnAroundTrigger");
+    }
+    
+    public void moveForward()
+    {
+        GetComponent<Animator>().SetTrigger("walkingTrigger");
+    }
+
+    public void idle()
+    {
+        GetComponent<Animator>().SetTrigger("idleTrigger");
+    }
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("f"))
+        {
+            switchAnimation("leftMoveTrigger");
+            //Debug.Log("it works");
+            //leftMove();
+        } else if (Input.GetKey("g"))
+        {
+            turnAround();
+        } else if (Input.GetKey("h"))
+        {
+            rightMove();
+        } else if (Input.GetKey("t"))
+        {
+            moveForward();
+        } else
+        {
+            idle();
+        }
+    }
+}
